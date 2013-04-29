7747b8781712da638c30b2ced7c1bb4878dc41ec
DrWhatNoName <drwhat@cryto.net>
2013-04-29 16:44:45 +0100

    Small DB optimization
    Signed-off-by: DrWhatNoName <drwhat@cryto.net>

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,7 +1,37 @@
 <?php
-$hostname_modulatemedia = "localhost";// Database hostname or ip
-$database_modulatemedia = "evomap"; //Database name
-$username_modulatemedia = "localuser"; //Database user
-$password_modulatemedia = "localpassword"; //Database password
-$modulatemedia = mysql_pconnect($hostname_modulatemedia, $username_modulatemedia, $password_modulatemedia) or trigger_error(mysql_error(),E_USER_ERROR); 
+/*
+Eclipse Distribution License - v 1.0
+
+Copyright (c) 2007, Eclipse Foundation, Inc. and its licensors.
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+    Neither the name of the Eclipse Foundation, Inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+$config['db'] = array(
+	'host' 	=> 	'localhost',	//Your mysql hostname
+	'user' 	=> 	'root',			//Your mysql user
+	'db' 	=> 	'evomap',		//Your mysql Database
+	'pass'	=> 	''				//Your mysql Password
+);
+$debug 	= 0; //Are we debuigging a problem?
+$maint 	= 0; //Are we doing some work?
+
+
+$db = new PDO( 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['db'] , $config['db']['user'], $config['db']['pass'] );
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if ($debug){
+	$PDOstatement = array(
+			'title' 	=> 'DEBUGGING',
+			'error' 	=> '<br><pre>', print_r($query->errorInfo()), '<pre>' );
+}
+
+
 ?>
