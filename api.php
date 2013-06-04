10fa1f8f707480b2710ba5957b9da671757a5802
DrWhatNoName <drwhat@cryto.net>
2013-06-04 21:08:05 +0100

    building up API
    Signed-off-by: DrWhatNoName <drwhat@cryto.net>

--- api.php
+++ api.php
@@ -1,19 +1,27 @@
 <?php
-header('Content-type: text/javascript');
+include('include/search.inc.php');
+
 if (isset($_GET['API']) && $_GET['API'] = 'JSONAPI') {
 	if ($_GET['user'] = 'botdonator') {
-		if (isset($_POST['lord']) && isset($_POST['x']) && isset($_POST['lord'])) {
-			echo $_POST['lord'], $_POST['x'], $_POST['y'];
-		} else {
-			if (!isset($_POST['lord'])){
-				echo 'E01';
-			} else if (!isset($_POST['z'])){
-				echo 'E03';
-			} else if (!isset($_POST['y'])){
-				echo 'E02';
-			} else if (!isset($_POST[''])){
-				echo 'E04';
-			}
+		if (!isset($_POST['lord'])){
+			echo 'E01';
+		} else if (!isset($_POST['x'])){
+			echo 'E03';
+		} else if (!isset($_POST['y'])){
+			echo 'E02';
+		} else if (!isset($_POST['sid'])){
+			echo 'E05';
+		} else if (isset($_POST['lord'], $_POST['x'], $_POST['lord'], $_POST['sid'])) {
+		$data = array(
+			'SID' => $_POST['sid'],
+			'lord' => $_POST['lord'],
+			'xxx' => $_POST['x'],
+			'zzz' => $_POST['y']
+			);
+		$data = json_encode($data);
+		echo $data;
+		$data = json_decode($data, true);
+		echo '<pre>', print_r($data), '<pre>';
 		}
 	} else {
 		echo 'E98: Please use a VALID API user!';
