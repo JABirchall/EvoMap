a5a45254d8b878a80d851eb3fecc66791afe4e8b
DrWhatNoName <drwhat@cryto.net>
2013-06-04 21:34:13 +0100

    API error reporting
    Signed-off-by: DrWhatNoName <drwhat@cryto.net>

--- api.php
+++ api.php
@@ -1,8 +1,12 @@
 <?php
 include('include/search.inc.php');
 
-if (isset($_GET['API']) && $_GET['API'] = 'JSONAPI') {
-	if ($_GET['user'] = 'botdonator') {
+if (!isset($_GET['API']) && @$_GET['API'] != 'JSONAPI') {
+	echo 'E99: Please use your API user to gain access!';
+} else {
+	if (@$_GET['user'] != 'botdonator') {
+		echo 'E98: Please use a VALID API user!';
+	} else {
 		if (!isset($_POST['lord'])){
 			echo 'E01';
 		} else if (!isset($_POST['x'])){
@@ -16,18 +20,14 @@ if (isset($_GET['API']) && $_GET['API'] = 'JSONAPI') {
 			'SID' => $_POST['sid'],
 			'lord' => $_POST['lord'],
 			'xxx' => $_POST['x'],
-			'zzz' => $_POST['y']
+			'yyy' => $_POST['y']
 			);
 		$data = json_encode($data);
 		echo $data;
 		$data = json_decode($data, true);
 		echo '<pre>', print_r($data), '<pre>';
 		}
-	} else {
-		echo 'E98: Please use a VALID API user!';
 	}
-} else {
-	echo 'E99: Please use your API user to gain access!';
 }
 
 ?>
