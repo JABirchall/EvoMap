5e2b8636cfad39dcdf384f7ae3ff575c15e8b381
DrWhatNoName <drwhat@cryto.net>
2013-06-08 17:53:30 +0100

    Json input
    Signed-off-by: DrWhatNoName <drwhat@cryto.net>

--- api.php
+++ api.php
@@ -1,51 +1,36 @@
 <?php
 header('content-type: text/javascript');
 include('include/search.inc.php');
-if (!isset($_GET['API']) && @$_GET['API'] != 'JSONAPI')
-{
+if (!isset($_GET['API']) && @$_GET['API'] != 'JSONAPI'){
 	echo 'E99: Please use your API user to gain access!';
-}
-else
-{
-	if (@$_GET['user'] != 'botdonator')
-	{
+}else{
+	if (@$_GET['user'] != 'botdonator'){
 		echo 'E98: Please use a VALID API user!';
-	}
-	else
-	{
-		if (!isset($_POST['json']))
-		{
+	}else{
+		if (!isset($_POST['json'])){
 			echo 'E01';
-		} else if (!isset($_GET['x']))
-		{
+		} else if (!isset($_POST['sid'])){
 			echo 'E02';
-		} else if (!isset($_GET['y']))
-		{
-			echo 'E03';
-		}
-		else if (isset($_POST['json']))
-		{
+		} else if (isset($_POST['json'])){
 			$data = json_decode($_POST['json']);
-			$data->x = $_GET['x'];
-			$data->y = $_GET['y'];
-			print_r($data);
-		}
+			foreach ($data as $key => $data) {
+				$data->x = intval($data->id % 800);
+				$data->y = intval($data->id / 800);
+				$data->sid = intval($POST['sid']);
 		try {
-				$api_submit->bindValue(':sid', $_POST['sid'], PDO::PARAM_INT);
-				$api_submit->bindValue(':lord', $_POST['lord'], PDO::PARAM_STR);
-				$api_submit->bindValue(':xxx', $_POST['x'], PDO::PARAM_INT);
-				$api_submit->bindValue(':yyy', $_POST['y'], PDO::PARAM_INT);
-				$api_submit->bindValue(':city', $_POST['city'], PDO::PARAM_STR);
-				$api_submit->bindValue(':alliance', $_POST['alliance'], PDO::PARAM_STR);
-				$api_submit->bindValue(':flag', $_POST['flag'], PDO::PARAM_STR);
-				$api_submit->bindValue(':honor', $_POST['honor'], PDO::PARAM_INT);
-				$api_submit->bindValue(':prestige', $_POST['prestige'], PDO::PARAM_INT);
+				$api_submit->bindValue(':sid', $data->sid, PDO::PARAM_INT);
+				$api_submit->bindValue(':lord', $data->userName, PDO::PARAM_STR);
+				$api_submit->bindValue(':xxx', $data->x, PDO::PARAM_INT);
+				$api_submit->bindValue(':yyy', $data->y, PDO::PARAM_INT);
+				$api_submit->bindValue(':city', $data->name, PDO::PARAM_STR);
+				$api_submit->bindValue(':alliance', $data->allianceName, PDO::PARAM_STR);
+				$api_submit->bindValue(':flag', $data->flag, PDO::PARAM_STR);
+				$api_submit->bindValue(':honor', $data->honor, PDO::PARAM_INT);
+				$api_submit->bindValue(':prestige', $data->prestige, PDO::PARAM_INT);
 				$api_submit->execute();
 				echo 'AOK';
 			} catch(PDOException $e){
 				die($e->getMessage());
 			}
-		
-	}
-}
+}}}}
 ?>
